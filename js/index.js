fad8dc6eb5fe1a4a645d9bccd6ace8bcfa7f90c5
Douglas Dotto <douglasdotto@hotmail.com.br>
2024-05-01 12:46:49 -0300

    ajustes e fontes

--- js/index.js
+++ js/index.js
@@ -1,36 +1,42 @@
 const levels = [
     {
-        question: 'Mercado Livre é a empresa de tecnologia líder na América Latina',
+        question: 'Mercado Livre é a empresa de tecnologia líder na América Latina <img src="./images/1.png" />',
         answer: 'verdade',
         explanation: 'Em 2023, a marca foi considerada uma das 100 mais influentes do mundo pela revista TIME e uma das companhias que mudam o mundo pela Fortune.No mesmo ano, foi condecorada com o Responsible Business Award, da Reuters, por seu cuidado altamente elogiado à biodiversidade. Atualmente, o Mercado Livre emprega 58 mil colaboradores.'
     },
     {
-        question: 'O e-commerce do Mercado Livre não possui o envio mais eficiente',
+        question: 'O e-commerce do Mercado Livre não possui o envio mais eficiente <img src="./images/2.png" />',
         answer: 'mito',
         explanation: 'Na liderança do e-commerce em toda América Latina, o Mercado Livre oferece serviço completo e sortimento acessível tanto para compradores quanto vendedores. A eficiência se comprova nos dados do quarto trimestre de 2023, que surpreendem: foram 407 milhões de itens enviados e 413 milhões de itens vendidos.'
     },
     {
-        question: 'O Mercado Livre e o Mercado Pago não são da mesma companhia',
+        question: 'O Mercado Livre e o Mercado Pago não são da mesma companhia <img src="./images/3.png" />',
         answer: 'mito',
         explanation: 'O Mercado Pago é o banco digital do Mercado Livre, que também lidera em serviços financeiros na América Latina. São 53 milhões de usuários únicos ativos em toda a região.'
     },
     {
-        question: 'O Mercado Livre promove inclusão financeira',
+        question: 'O Mercado Livre promove inclusão financeira <img src="./images/4.png" />',
         answer: 'verdade',
-        explanation: 'Empoderar pessoas e impulsionar negócios vem sendo um pilar essencial para a companhia. Para se ter uma ideia, 26 novos postos de empregos diretos e indiretos são criados por hora e + de 1,8 milhão de famílias têm sua principal fonte de renda através do Mercado Livre. Fonte: Euromonitor, Impactos que Importam, 2023.'
+        explanation: 'Empoderar pessoas e impulsionar negócios vem sendo um pilar essencial para a companhia. Para se ter uma ideia, 26 novos postos de empregos diretos e indiretos são criados por hora e + de 1,8 milhão de famílias têm sua principal fonte de renda através do Mercado Livre.<br/><br/><small>Fonte: Euromonitor, Impactos que Importam, 2023.</small>'
     },
     {
-        question: 'Para o Mercado Livre, os impactos sociais e ambientais positivos vêm em primeiro lugar',
+        question: 'Para o Mercado Livre, os impactos sociais e ambientais positivos vêm em primeiro lugar <img src="./images/5.png" />',
         answer: 'verdade',
         explanation: 'A redução dos impactos ambientais começa desde a frota, que conta com 2,4 mil veículos movidos à gás natural. Segundo o Relatório de Impacto (2023), foram vendidos +10 milhões de produtos de impacto positivo na plataforma, entre eles bicicletas e garrafas reutilizáveis. Mais de 22 milhões de pacotes enviados através de modais sustentáveis e +13 mil árvores nativas restauradas. Na esfera de equidade social, 28% de mulheres ocupam cargos de liderança e 48% de pessoas negras no Brasil. 12% dos colaboradores se identificam como LGBTQIA+.'
     }
 ];
 
 let currentLevelIndex = 0;
+let score = 0;
 
+const quizIndex = document.getElementById('quiz-index');
 const questionContainer = document.getElementById('question-container');
 const explanationContainerTrue = document.getElementById('explanation-container-true');
 const explanationContainerFalse = document.getElementById('explanation-container-false');
+const imgCloseTrue = document.getElementById('img-true-close');
+const imgCheckTrue = document.getElementById('img-true-check');
+const imgCloseFalse = document.getElementById('img-false-close');
+const imgCheckFalse = document.getElementById('img-false-check');
 const nextButton = document.getElementById('next-button');
 const trueButton = document.getElementById('true-button');
 const falseButton = document.getElementById('false-button');
@@ -41,32 +47,59 @@ function showQuestion() {
     explanationContainerTrue.style.display = 'none';
     explanationContainerFalse.style.display = 'none';
 
+    quizIndex.innerHTML = (currentLevelIndex + 1) + '/5';
     explanationContainerTrue.innerHTML = '';
     explanationContainerFalse.innerHTML = '';
+    imgCloseTrue.style.display = 'none';
+    imgCheckTrue.style.display = 'none';
+    imgCloseFalse.style.display = 'none';
+    imgCheckFalse.style.display = 'none';
     nextButton.style.display = 'none';
+
+    trueButton.style.color = "var(--text-primary)";
+    falseButton.style.color = "var(--text-primary)";
+    trueButton.classList.remove("btn-black");
+    falseButton.classList.remove("btn-black");
     trueButton.style.display = 'block';
     falseButton.style.display = 'block';
+    trueButton.disabled = false;
+    falseButton.disabled = false;
 }
 
 function checkAnswer(answer) {
     const currentLevel = levels[currentLevelIndex];
     const isCorrect = answer === currentLevel.answer;
 
+    if (isCorrect)
+        score = +1;
+
     if (currentLevel.answer == 'verdade') {
         explanationContainerTrue.innerHTML = currentLevel.explanation;
         explanationContainerTrue.style.display = 'block';
+        trueButton.style.color = "var(--confirmation-btn)";
     }
     else {
         explanationContainerFalse.innerHTML = currentLevel.explanation;
         explanationContainerFalse.style.display = 'block';
+        falseButton.style.color = "var(--confirmation-btn)";
     }
 
-    if (!isCorrect) {
-        console.log('Resposta incorreta!');
+    if (currentLevel.answer == 'mito' && answer == 'verdade') {
+        trueButton.classList.add("btn-black");
+        imgCloseTrue.style.display = 'block';
+    } else if (currentLevel.answer == 'verdade' && answer == 'verdade') {
+        imgCheckTrue.style.display = 'block';
+    } else if (currentLevel.answer == 'verdade' && answer == 'mito') {
+        falseButton.classList.add("btn-black");
+        imgCloseFalse.style.display = 'block';
+    } else if (currentLevel.answer == 'mito' && answer == 'mito') {
+        imgCheckFalse.style.display = 'block';
     }
 
     trueButton.style.display = 'block';
     falseButton.style.display = 'block';
+    trueButton.disabled = true;
+    falseButton.disabled = true;
     nextButton.style.display = 'block';
 }
 
@@ -75,7 +108,7 @@ function goToNextLevel() {
     if (currentLevelIndex < levels.length) {
         showQuestion();
     } else {
-        alert('Quiz concluído!');
+        finish()
     }
 }
 
@@ -91,8 +124,15 @@ nextButton.addEventListener('click', () => {
     if (currentLevelIndex < levels.length) {
         goToNextLevel();
     } else {
-        alert('Quiz concluído!');
+        finish()
     }
 });
+
+function finish() {
+    trueButton.style.display = 'none';
+    falseButton.style.display = 'none';
+    nextButton.style.display = 'none';
+    quizIndex.innerHTML = 'Resultado';
+}
 
 showQuestion();
